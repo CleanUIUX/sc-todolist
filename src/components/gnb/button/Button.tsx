ed82332ecfda9a8efbe5eda22266d459f5791f32
SeungChan-Roh <ox043440@gmail.com>
2024-11-28 00:40:08 +0900

    fit: button 속성수정

--- src/components/gnb/button/Button.tsx
+++ src/components/gnb/button/Button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps {
     title: string,
     bgColor: string,
     textColor: string,
+    customClass: "",
     onClick?:() => void; //클릭 이벤트 핸들러
 }
 
@@ -11,12 +12,13 @@ export const Button = ({
    title,
    bgColor,
    textColor,
+   customClass,
    onClick, 
 }:ButtonProps) => {
     return(
         <button
-            className={`flex justify-center items-center py-15 px-30 rounded-full ${bgColor} ${textColor}`}
+            className={`flex justify-center items-center py-15 px-30 rounded-full ${bgColor} ${textColor} ${customClass}`}
             onClick={onClick}
-        ></button>
+        >{title}</button>
     )
 }
