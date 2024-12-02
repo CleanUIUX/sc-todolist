04748778c13e5deb95e3af167c54233481acdb32
SeungChan-Roh <ox043440@gmail.com>
2024-12-03 00:22:08 +0900

    style: first Tab 작업중

--- src/components/gnb/superlist/TabAppLaunch.tsx
+++ src/components/gnb/superlist/TabAppLaunch.tsx
@@ -2,10 +2,14 @@
 
 export const TabAppLaunch = () => {
     return (
-        <div>
-            <h2>TabAppLaunch</h2>
+        <div className="flex flex-col p-6 border-2 rounded-[32px] bg-white">
+            <h3>App Launch</h3>
+            <p>Hey team, here's a quick recap of what we're working on ahead of the launch. Feel free to add any more notes or details to the tasks.</p>
+            <div>
+                <div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
-
-export default TabAppLaunch;
