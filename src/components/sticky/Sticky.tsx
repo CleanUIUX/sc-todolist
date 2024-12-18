56b2b9006dff97358f8c58d5331426d8afa7cb87
SeungChan-Roh <ox043440@gmail.com>
2024-12-19 00:08:12 +0900

    feat: video 추가

--- src/components/sticky/Sticky.tsx
+++ src/components/sticky/Sticky.tsx
@@ -2,16 +2,43 @@
 
 export const Sticky = () => {
   return(
-    <>
-    <div className="h-60 bg-slate-300">
-      <p>section1</p>
+  <section className="max-w-[1250px] mx-auto">
+    <div className="h-60 bg-slate-300 sticky top-0 z-10">
+      <video 
+          className="w-full h-60 pointer-events-none" 
+          controls 
+          autoPlay 
+          loop 
+          muted
+        >
+          <source src="/assets/video1.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
     </div>
-    <div className="h-60 bg-slate-400">
-      <p>section2</p>
+    <div className="h-60 bg-slate-400 sticky top-0 z-10">
+      <video 
+          className="w-full h-60 pointer-events-none" 
+          controls 
+          autoPlay 
+          loop 
+          muted
+        >
+          <source src="/assets/video2.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>  
     </div>
-    <div className="h-60 bg-slate-500">
-      <p>section3</p>
+    <div className="h-60 bg-slate-500 sticky top-0 z-10">
+      <video 
+          className="w-full h-60 pointer-events-none" 
+          controls 
+          autoPlay 
+          loop 
+          muted
+        >
+          <source src="/assets/video3.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>  
     </div>
-    </>
+  </section>
   )
 }
