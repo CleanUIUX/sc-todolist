01bd9f3b2c63a201a6f1b260c4cf25be647d1b01
SeungChan-Roh <ox043440@gmail.com>
2024-12-18 23:50:02 +0900

    feat: sticky재수정

--- src/components/sticky/Sticky.tsx
+++ src/components/sticky/Sticky.tsx
@@ -1,21 +1,17 @@
 'use client';
 
-interface StickyProps {
-  children: React.ReactNode;
-  top?: string; // Sticky 위치를 설정할 수 있는 옵션
-}
-
-export const Sticky: React.FC<StickyProps> = ({ children, top = '0px' }) => {
-  return (
-    <div
-      className="sticky"
-      style={{
-        position: 'sticky',
-        top: top,
-        zIndex: 10, // Optional: 다른 요소 위에 위치하도록 설정
-      }}
-    >
-      {children}
+export const Sticky = () => {
+  return(
+    <>
+    <div className="h-60 bg-slate-300">
+      <p>section1</p>
     </div>
-  );
-};
+    <div className="h-60 bg-slate-400">
+      <p>section2</p>
+    </div>
+    <div className="h-60 bg-slate-500">
+      <p>section3</p>
+    </div>
+    </>
+  )
+}
